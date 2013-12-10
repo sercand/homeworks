94e9c8021345880b304bfe0df1067583bb546906
Sercan Değirmenci <degirmencisercan@gmail.com>
2013-12-10 17:16:27 +0200

    Bir şey değişmedi

--- THE2.py
+++ THE2.py
@@ -20,30 +20,19 @@ def keep_left(h, r): #find next valid point on convex hull.
     if not len(h) or h[-1] != r: h.append(r)
     return h
 
-def calc_circle(v1,v2,v3):  #calculate a circle from 3 points
-    aSlope = (v2[1] - v1[1])/(v2[0] - v1[0] + 0.000000001)
-    bSlope = (v3[1] - v2[1])/(v3[0] - v2[0] + 0.000000001)
-    if fabs(aSlope-bSlope) <= 0.000000001: return -1  #points are in same line
-    mx = (aSlope * bSlope * (v1[1] - v3[1]) + bSlope*(v1[0] + v2[0]) -
-          aSlope * (v2[0] + v3[0])) / (2 * (bSlope - aSlope))
-    my = -1 * (mx - (v1[0] + v2[0]) / 2) / aSlope + (v1[1] + v2[1]) / 2
-    return [mx, my, length([mx, my], v1)]  #returns x of center, y of center and radius of circle
-
-def check_line(points):#check whether 4 points in a line or not
-    m1, m2 = slope(points[0], points[1]), slope(points[0], points[2])
-    return compare(m1, m2) and compare(m1, slope(points[0], points[3]))
-
-def check_circle(points):  #check if 4 points in a circle. Points must be CCW order
-    circle = calc_circle(points[0], points[1], points[2]) #calculate circle
-    if circle == -1 or not compare(length(points[3], circle), circle[2]): return False  #this is not circle
-    return True  #this is a circle, because three points means a circle and forth point is on the circle
-
 def geo_wizard(points):
-    if check_line(points): return 'line'
+    m1, m2 = slope(points[0], points[1]), slope(points[0], points[2])
+    if compare(m1, m2) and compare(m1, slope(points[0], points[3])):  return 'line'
     points = sorted(points)
     l = reduce(keep_left, points, [])
     u = reduce(keep_left, points[::-1], [])
     ccw = l.extend(u[i] for i in xrange(1, len(u) - 1)) or l
     if len(ccw) != len(points): return 'arbitraryquadrilateral'
-    if check_circle(ccw): return 'circle'
+    aSlope = (ccw[1][1] - ccw[0][1])/(ccw[1][0] - ccw[0][0] + 0.000000001)
+    bSlope = (ccw[2][1] - ccw[1][1])/(ccw[2][0] - ccw[1][0] + 0.000000001)
+    if fabs(aSlope-bSlope) <= 0.000000001: return 'triangle'
+    mx = (aSlope * bSlope * (ccw[0][1] - ccw[2][1]) + bSlope*(ccw[0][0] + ccw[1][0]) -
+          aSlope * (ccw[1][0] + ccw[2][0])) / (2 * (bSlope - aSlope))
+    my = -1 * (mx - (ccw[0][0] + ccw[1][0]) / 2) / aSlope + (ccw[0][1] + ccw[1][1]) / 2
+    if compare(length([mx, my], ccw[0]), length(ccw[3], [mx, my])): return 'circle'
     return 'triangle'
