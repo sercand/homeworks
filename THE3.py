a6100bf9b535004619c16a8a4c129277f446a47b
Sercan Değirmenci <degirmencisercan@gmail.com>
2013-12-29 11:37:29 +0200

    Small Fixes

--- THE3.py
+++ THE3.py
@@ -1,12 +1,14 @@
 #Created by Sercan Degirmenci on 28.12.2013
 from math import sqrt
+inf = float("inf")
 
 def length(v1, v2):
-    return sqrt((v1[0] - v2[0]) * (v1[0] - v2[0]) + (v1[1] - v2[1]) * (v1[1] - v2[1]))
+    da, db = (v1[0] - v2[0]), (v1[1] - v2[1])
+    return sqrt(da * da + db * db)
 
 def do_it(points, index):
     if len(points) == 1 : return 0 , points
-    p0, j, shortest = points.pop(index), 0, float("inf")
+    p0, j, shortest = points.pop(index), 0, inf
     for i in xrange(0, len(points)):
         d = length(p0, points[i])
         if d < shortest: j, shortest = i, d
@@ -14,7 +16,7 @@ def do_it(points, index):
     return shortest + new[0], [p0] + new[1]
 
 def order(points):
-    shortest, points = (float("inf"), []), sorted(points)
+    shortest, points = (inf, []), sorted(points)
     for i in xrange(0, len(points)):
         temp = do_it(points[:], i)
         if temp[0] < shortest[0]: shortest = temp
