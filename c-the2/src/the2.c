fa33ff8df0078714e12182fa1dda0d2db45f91d1
Sercan Değirmenci <degirmencisercan@gmail.com>
2014-04-30 01:18:44 +0300

    reading and initializing functions are added

--- c-the2/src/the2.c
+++ c-the2/src/the2.c
@@ -2,60 +2,194 @@
 	degirmencisercan@gmailcom					*/
 #include <stdlib.h>
 #include <stdio.h>
-/*
-&  And
-|  OR
--  Not
->  -> ise
-=  isEqual
-(  open bracket
-)  close bracket
-*/
-
-#define EXP(e) int exp##e(char* c,int n);
+#include <ctype.h>
+#define FALSE 0
+#define TRUE 1
+#define bool int
+#define string char*
+#define EXP(e) bool exp##e(string c,int n);
 EXP(And)/* & */
 EXP(Or)/* | */
 EXP(Not)/* - */
 EXP(IfThen)/* > */
 EXP(IsEqual)/* = */
-int expression(char* c, int n);
-int resize_input(char *c, int n);/* Returns new size of input*/
-int * characters;
-int ** inputs;
-char * input;
-int index_of_input = 1, size_of_chacters = 0;
+int expression(string c, int n); /* run an expression such as (x & y) or (x | y) */
+int resize_input(string c, int n);/* returns new size of input*/
+int has_input_character(int c);
+void init_characters();
+void init_inputs();
+void print_result();
+void sort_characters();
+string characters; /* all characters in input*/
+int ** inputs; /*an array consist all inputs. inputs[n][n*n] */
+string input;
+int * results;
+int index_of_input = 1, size_of_characters = 0, size_of_input = 0;
 
 int main(int argc, char ** argv)
 {
-	
+	char a;
+	int i;
+	input = (char*)malloc(0);
+	while (1)
+	{
+		a = getchar();
+		size_of_input++;
+		input = realloc(input, size_of_input*sizeof(char));
+		if (a != -1)
+		{
+			input[size_of_input - 1] = a;
+		}
+		else
+		{
+			input[size_of_input - 1] = 0;
+			break;
+		}
+	}
+	size_of_input = resize_input(input, size_of_input);
+	init_characters();
+	init_inputs();
+	for (i = 1; i <= size_of_characters*size_of_characters; i++)
+	{
+		index_of_input = i;
+		results[i - 1] = expression(input, size_of_input);
+	}
+	print_result();
 	return 0;
 }
 
-int expAnd(char* c, int n)
+bool expAnd(string c, int n)
 {
-	return 0;
+	return FALSE;
 }
-int expOr(char* c, int n)
+bool expOr(string c, int n)
 {
-	return 0;
+	return FALSE;
 }
-int expNot(char* c, int n)
+bool expNot(string c, int n)
 {
-	return 0;
+	return FALSE;
 }
-int expIfThen(char* c, int n)
+bool expIfThen(string c, int n)
 {
-	return 0;
+	return FALSE;
 }
-int expIsEqual(char* c, int n)
+bool expIsEqual(string c, int n)
 {
-	return 0;
+	return FALSE;
 }
-int expression(char* c, int n)
+bool expression(string c, int n)
 {
-	return 0;
+	return FALSE;
 }
-int resize_input(char *c, int n)
+int resize_input(string c, int n)
 {
-	return 0;
+	int i = 0, j = 0, t = 0;
+	char * newi;
+	for (; j < n; j++)
+	{
+		char cc = c[j];
+		if (cc != ' '&&cc != '\n'&&cc != '\t'&&cc != '\r')
+			i++;
+	}
+	newi = malloc(sizeof(char)*i);
+	for (j = 0; j < n; j++)
+	{
+		char cc = c[j];
+		if (cc != ' '&&cc != '\n'&&cc != '\t'&&cc != '\r'){
+			newi[t] = cc;
+			t++;
+		}
+	}
+	input = newi;
+	return i;
+}
+void init_characters()
+{
+	int i = 0;
+	for (; i < size_of_input; i++)
+	{
+		if (islower(input[i]) && !has_input_character(input[i]))
+		{
+			size_of_characters++;
+			characters = realloc(characters, size_of_characters*sizeof(char));
+			characters[size_of_characters - 1] = input[i];
+		}
+	}
+	sort_characters();
+}
+void init_inputs()
+{
+	int i = 0, j, k, l, m, n, t = 1;
+	inputs = (int **)malloc(size_of_characters*sizeof(int*));
+	n = size_of_characters*size_of_characters + 1;
+	for (i = 0; i < size_of_characters; i++)
+	{
+		inputs[i] = (int*)malloc(n*sizeof(int));
+	}
+	results = (int*)malloc(n*sizeof(int));
+
+	for (i = 0; i < size_of_characters; i++)
+	{
+		j = (size_of_characters - i) - 1;
+		k = 1 << i;
+		inputs[j][0] = characters[j];
+		t = 1;
+		for (l = 1; l < n;)
+		{
+			for (m = 0; m < k && l < n; m++, l++)
+			{
+				inputs[j][l] = t;
+			}
+			t = !t;
+		}
+	}
+}
+void print_result()
+{
+	int i = 0, j = 0, n = size_of_characters*size_of_characters;
+	for (; i < size_of_characters; i++)
+	{
+		printf("%c ", characters[i]);
+	}
+	printf("R\n");
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < size_of_characters; j++)
+		{
+			printf("%c ", inputs[j][i + 1] ? 'T' : 'F');
+		}
+		printf("%c\n", results[i] ? 'T' : 'F');
+	}
+}
+int has_input_character(int c)
+{
+	int i = 0;
+
+	for (; i < size_of_characters; i++)
+	{
+		if (characters[i] == c)
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+void sort_characters()
+{
+	int i, j, temp;
+
+	for (i = 0; i < (size_of_characters - 1); i++)
+	{
+		for (j = 0; j < size_of_characters - i - 1; j++)
+		{
+			if (characters[j] > characters[j + 1])
+			{
+				temp = characters[j];
+				characters[j] = characters[j + 1];
+				characters[j + 1] = temp;
+			}
+		}
+	}
 }
