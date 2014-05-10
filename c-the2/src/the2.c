c9ae1def905e53812499dcda0de94ed15f6f739f
Sercan Değirmenci <degirmencisercan@gmail.com>
2014-05-10 23:22:04 +0300

    expressions added and first fully working version

--- c-the2/src/the2.c
+++ c-the2/src/the2.c
@@ -7,185 +7,149 @@
 #define TRUE 1
 #define bool int
 #define string char*
-#define EXP(e) bool exp##e(string c,int n);
-EXP(And)/* & */
-EXP(Or)/* | */
-EXP(Not)/* - */
-EXP(IfThen)/* > */
-EXP(IsEqual)/* = */
-int expression(string c, int n); /* run an expression such as (x & y) or (x | y) */
+bool expression(string c, int start, int *end); /* run an expression such as (x & y) or (x | y) */
 int resize_input(string c, int n);/* returns new size of input*/
-int has_input_character(int c);
+int index_of_character(int c);
 void init_characters();
-void init_inputs();
+void init_cases();
 void print_result();
 void sort_characters();
 string characters; /* all characters in input*/
-int ** inputs; /*an array consist all inputs. inputs[n][n*n] */
+int ** cases; /*an array consist all cases. cases[n][n*n] */
 string input;
 int * results;
-int index_of_input = 1, size_of_characters = 0, size_of_input = 0;
+int index_of_test = 0, size_of_characters = 0, size_of_input = 0, size_of_cases = 0;
 
 int main(int argc, char ** argv)
 {
-	char a;
-	int i;
+	int a, i, n = 0;
 	input = (char*)malloc(0);
 	while (1)
 	{
 		a = getchar();
 		size_of_input++;
 		input = realloc(input, size_of_input*sizeof(char));
-		if (a != -1)
-		{
+		if (a != EOF){
 			input[size_of_input - 1] = a;
 		}
-		else
-		{
+		else{
 			input[size_of_input - 1] = 0;
 			break;
 		}
 	}
 	size_of_input = resize_input(input, size_of_input);
 	init_characters();
-	init_inputs();
-	for (i = 1; i <= size_of_characters*size_of_characters; i++)
-	{
-		index_of_input = i;
-		results[i - 1] = expression(input, size_of_input);
+	init_cases();
+	for (i = 0; i < size_of_cases; i++){
+		index_of_test = i;
+		results[i] = expression(input, 0, &n);
 	}
 	print_result();
 	return 0;
 }
 
-bool expAnd(string c, int n)
+bool expression(string s, int start, int *end)
 {
+	int res1, res2, out, exp;
+	if (s[start] == '-'){
+		return !(expression(s, start + 1, end));
+	}
+	else if (s[start] == '('){
+		res1 = expression(s, start + 1, &out);
+		exp = s[out];
+		res2 = expression(s, out + 1, &out);
+		(*end) = out + 1;
+		if (exp == '&') return res1 && res2;
+		else if (exp == '|') return res1 || res2;
+		else if (exp == '=') return res1 == res2;
+		else if (exp == '>') return (!res1) || res2;
+	}
+	else if (index_of_character(s[start]) > -1){
+		(*end) = start + 1;
+		return cases[index_of_character(s[start])][index_of_test];
+	}
 	return FALSE;
 }
-bool expOr(string c, int n)
-{
-	return FALSE;
-}
-bool expNot(string c, int n)
-{
-	return FALSE;
-}
-bool expIfThen(string c, int n)
-{
-	return FALSE;
-}
-bool expIsEqual(string c, int n)
-{
-	return FALSE;
-}
-bool expression(string c, int n)
-{
-	return FALSE;
-}
-int resize_input(string c, int n)
+
+int resize_input(string s, int n)
 {
 	int i = 0, j = 0, t = 0;
-	char * newi;
-	for (; j < n; j++)
-	{
-		char cc = c[j];
-		if (cc != ' '&&cc != '\n'&&cc != '\t'&&cc != '\r')
-			i++;
+	string newi;
+	for (; j < n; j++) if (s[j] > 32) i++; /* Count number of invalid chars */
+	newi = malloc(sizeof(char)*(i + 1)); /* Init new init string. +1 is for last element */
+	for (j = 0; j < n; j++)	{
+		if (s[j] > 32) newi[t++] = s[j]; /*If cc is bigger than 32 than it is valid*/
 	}
-	newi = malloc(sizeof(char)*i);
-	for (j = 0; j < n; j++)
-	{
-		char cc = c[j];
-		if (cc != ' '&&cc != '\n'&&cc != '\t'&&cc != '\r'){
-			newi[t] = cc;
-			t++;
-		}
-	}
+	newi[t] = 0; /* Don not forget last item*/
 	input = newi;
 	return i;
 }
+
 void init_characters()
 {
 	int i = 0;
-	for (; i < size_of_input; i++)
-	{
-		if (islower(input[i]) && !has_input_character(input[i]))
-		{
+	for (; i < size_of_input; i++){
+		if (islower(input[i]) && index_of_character(input[i]) == -1){
 			size_of_characters++;
 			characters = realloc(characters, size_of_characters*sizeof(char));
 			characters[size_of_characters - 1] = input[i];
 		}
 	}
+	characters = realloc(characters, (size_of_characters + 1)*sizeof(char));
+	characters[size_of_characters] = 0;
 	sort_characters();
 }
-void init_inputs()
-{
-	int i = 0, j, k, l, m, n, t = 1;
-	inputs = (int **)malloc(size_of_characters*sizeof(int*));
-	n = size_of_characters*size_of_characters + 1;
-	for (i = 0; i < size_of_characters; i++)
-	{
-		inputs[i] = (int*)malloc(n*sizeof(int));
-	}
-	results = (int*)malloc(n*sizeof(int));
 
-	for (i = 0; i < size_of_characters; i++)
-	{
+void init_cases()
+{
+	int i = 0, j, k, l, m, t = 1;
+	cases = (int **)malloc(size_of_characters*sizeof(int*));
+	size_of_cases = 1 << size_of_characters;
+	for (i = 0; i < size_of_characters; i++){
+		cases[i] = (int*)malloc(size_of_cases*sizeof(int));
+	}
+	results = (int*)malloc(size_of_cases*sizeof(int));
+	for (i = 0; i < size_of_characters; i++){
 		j = (size_of_characters - i) - 1;
 		k = 1 << i;
-		inputs[j][0] = characters[j];
 		t = 1;
-		for (l = 1; l < n;)
-		{
-			for (m = 0; m < k && l < n; m++, l++)
-			{
-				inputs[j][l] = t;
+		for (l = 0; l < size_of_cases;)	{
+			for (m = 0; m < k && l < size_of_cases; m++, l++){
+				cases[j][l] = t;
 			}
 			t = !t;
 		}
 	}
 }
+
 void print_result()
 {
-	int i = 0, j = 0, n = size_of_characters*size_of_characters;
+	int i = 0, j = 0;
 	for (; i < size_of_characters; i++)
-	{
 		printf("%c ", characters[i]);
-	}
 	printf("R\n");
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < size_of_cases; i++){
 		for (j = 0; j < size_of_characters; j++)
-		{
-			printf("%c ", inputs[j][i + 1] ? 'T' : 'F');
-		}
+			printf("%c ", cases[j][i] ? 'T' : 'F');
 		printf("%c\n", results[i] ? 'T' : 'F');
 	}
 }
-int has_input_character(int c)
+
+int index_of_character(int c)
 {
 	int i = 0;
-
-	for (; i < size_of_characters; i++)
-	{
-		if (characters[i] == c)
-		{
-			return TRUE;
-		}
+	for (; i < size_of_characters; i++){
+		if (characters[i] == c) return i;
 	}
-	return FALSE;
+	return -1;
 }
 
 void sort_characters()
 {
 	int i, j, temp;
-
-	for (i = 0; i < (size_of_characters - 1); i++)
-	{
-		for (j = 0; j < size_of_characters - i - 1; j++)
-		{
-			if (characters[j] > characters[j + 1])
-			{
+	for (i = 0; i < (size_of_characters - 1); i++){
+		for (j = 0; j < size_of_characters - i - 1; j++){
+			if (characters[j] > characters[j + 1]){
 				temp = characters[j];
 				characters[j] = characters[j + 1];
 				characters[j + 1] = temp;
